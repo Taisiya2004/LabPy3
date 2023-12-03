cccf3880cbce0007b512a1c59f6b1dd6edc10c61
Taya <taisiyachurkina2004@mail.ru>
2023-12-03 16:31:48 +0300

    end commit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import re
 
 def check_russian_postal_code(postal_code):
-    pattern = r'\b\d{6}\b'  # шаблон для российского почтового индекса (6 цифр)
+    pattern = r'\b\d{6}\b'  # шаблон для российского почтового индекса (6 цифр).
     return bool(re.match(pattern, postal_code))
 
-# Получение ввода от пользователя
+# Получение ввода от пользователя.
 user_input = input("Введите почтовый индекс России: ")
 
-# Проверка введенной строки
+# Проверка введенной строки.
 if check_russian_postal_code(user_input):
     print("Это корректный почтовый индекс России")
 else:
